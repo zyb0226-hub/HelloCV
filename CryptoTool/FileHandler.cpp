9e63652de835e026bc08cd7e6abd822244b1e700
zyb0226-hub <3113255371@qq.com>
2025-10-20 23:02:40 +0800

    Update FileHandler.cpp

--- CryptoTool/FileHandler.cpp
+++ CryptoTool/FileHandler.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 class FileHandler {
 public:
-    bool readFile(const string& path, string& content) {
+    static bool readFile(const string& path, string& content) {
        ifstream file(path);     //Use input file stream to read file
        if(!file) return false;
        content.assign((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());  //I don't know how to extract all content from a file,so DS taught me this.
        return true;
     }
-    bool writeFile(const string& path, const string& content) {
+    static bool writeFile(const string& path, const string& content) {
        ofstream file(path);     //Use output file stream to write file
        if(!file) return false;
        file << content;
